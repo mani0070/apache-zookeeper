ac00dabe5c64b9e26fe6bf793559a09a8c123895
Manimaran Chandrasekaran <mani@dinventive.com>
2016-12-17 06:33:33 +0000

    updated the tar extract

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,19 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName= 'apache-zookeeper'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://apache.mirror.anlx.net/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz'
-$url64      = ''
+$options = @{
+  unzipLocation = 'C:\tools\'
+}
 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU'
-  url           = $url
-  url64bit      = $url64
-
+$packageParameters = @{
+  packageName   = 'apache-zookeeper'
+  url           = 'http://apache.mirror.anlx.net/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz'
+  $tempDir    = "$toolsDir\temp"
+  url64bit      = ''
   softwareName  = 'apache-solr*'
-
   checksum      = ''
   checksumType  = 'sha256'
   checksum64    = ''
@@ -21,4 +17,25 @@ $packageArgs = @{
 
 
 }
-Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+
+function Set-ChocolateyPackageOptions {
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+        [hashtable] $options
+    )
+    $packageParameters = $env:chocolateyPackageParameters;
+ 
+    if ($packageParameters) {
+        $parameters = ConvertFrom-StringData -StringData $env:chocolateyPackageParameters.Replace(" ", "`n")
+        $parameters.GetEnumerator() | ForEach-Object {
+           $options[($_.Key)] = ($_.Value)
+        }
+    }
+}
+Set-ChocolateyPackageOptions $options
+Install-ChocolateyZipPackage $packageParameters['packageName'] $packageParameters['url'] $packageParameters['tempDir'] $packageParameters['url64']
+
+# Untar into tools folder
+Get-ChocolateyUnzip -PackageName $packageParameters['packageName'] -FileFullPath "$packageParameters['tempDir']" -Destination "$options['unzipLocation']"
+
+Export-CliXml -Path (Join-Path $PSScriptRoot 'options.xml') -InputObject $options
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -3,4 +3,7 @@
 $packageName = 'apache-zookeeper'
 $softwareName = 'apache-zookeeper*'
 
+$optionsFile = (Join-Path $PSScriptRoot 'options.xml')
+
 Uninstall-ChocolateyZipPackage $packageName
+Remove-Item (Join-Path $options['unzipLocation'] '') -Recurse -Force
